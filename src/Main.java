d3042960e41324751b39f479c57bcb956ef118c6
liuren <1036339332@qq.com>
2025-03-24 13:57:40 +0800

    idea to Github

--- src/Main.java
+++ src/Main.java
@@ -8,5 +8,6 @@ public class Main {
         System.out.println("第二次提交");
         System.out.println("dev 分支");
         System.out.println("第一次fix分支提交");
+        System.out.println("idea to Github");
     }
 }

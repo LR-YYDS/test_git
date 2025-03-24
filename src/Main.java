2f5af4aa4a8cf82416ee37ea95f91bfea8fa523e
liuren <1036339332@qq.com>
2025-03-24 10:34:30 +0800

    第一次提交

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+/**
+ * ${description}
+ **/
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("第一次提交");
+    }
+}

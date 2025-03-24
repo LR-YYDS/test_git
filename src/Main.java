485d7098b9cbcf3dfd8826baa4620c368d5a05cc
liuren <1036339332@qq.com>
2025-03-24 10:39:14 +0800

    第二次提交

--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("第一次提交");
+        System.out.println("第二次提交");
     }
 }

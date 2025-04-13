e2889a65433c0d63c0495dd43c991a5cc2f8f94a
LR-YYDS <143152175+LR-YYDS@users.noreply.github.com>
2025-04-13 13:09:44 +0800

    复习2

--- src/Main.java
+++ src/Main.java
@@ -16,5 +16,6 @@ public class Main {
         System.out.println("idea to github");
         System.out.println("idea to github 4");
          System.out.println("复习");
+         System.out.println("复习2");
     }
 }
